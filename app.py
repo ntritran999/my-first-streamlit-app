cb3a3889aaf4454fb141f4a06bdd094b5c386968
just a random guy <81624097+ntritran999@users.noreply.github.com>
2023-02-19 15:09:59 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("GIẢI PHƯƠNG TRÌNH BẬC NHẤT")
+a = st.number_input("Tham số a")
+b = st.number_input("Tham số b")
+
+if st.button("Giải"):
+    result = -b/a
+    st.success(f'Phương trình có 1 nghiệm {result}')
+
