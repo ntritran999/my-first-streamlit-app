9ccbd6d5b4ce5ac25d73b272027f2414de5c4ef7
just a random guy <81624097+ntritran999@users.noreply.github.com>
2023-03-12 15:10:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import pickle
+import numpy as np
 
 st.title("Revenue Prediction")
 x_new = st.number_input("Input temperature")
 
 if st.button("Predict"):
     model = pickle.load(open('model.pickle', "rb"))
-    y_new = model.predict(np.array(x_new).reshape(-1,1))
-    st.success(f'Phương trình có 1 nghiệm {y_new}')
-
+    y_new = model.predict(np.array(x_new).reshape(-1,1))[0]
+    st.write("Revenue Prediction")
+    st.success(f'{y_new}')
